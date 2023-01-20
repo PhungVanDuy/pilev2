547b2f9399bade720616e0891bf674b5cdab4c3a
Reshinth Adithyan <36307201+reshinthadithyan@users.noreply.github.com>
2023-01-20 22:37:06 +0530

    Adding shard len calculator

---
+++ pile/processing/dedup/stages_dedup/calc_len_per_shard_file.py
@@ -0,0 +1,34 @@
+import datasets
+import logging
+import argparse
+import os
+import json
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_file", type=str)
+    parser.add_argument("--output_file", type=str)
+    args = parser.parse_args()
+
+    minhash_dataset_path_dict = {}
+    minhash_dataset_list_file = args.input_file
+    with open(minhash_dataset_list_file, "r") as f:
+        lines = f.read().splitlines()
+        for line in lines:
+            minhash_dataset_path = line.strip()
+            # check if the path is valid and is a directory
+            assert os.path.isdir(minhash_dataset_path.strip())
+            # check if the path is a minhash dataset (ends with _minhash)
+            assert minhash_dataset_path.strip().endswith("_minhash")
+            # check if it is a hugging face dataset format by checking if it has a dataset_info.json
+            assert os.path.isfile(os.path.join(minhash_dataset_path.strip(), "dataset_info.json"))
+            minhash_dataset_path_dict[minhash_dataset_path] = len(datasets.load_from_disk(minhash_dataset_path.strip()))
+    
+    with open(args.output_file, "w") as f:
+        json.dump(minhash_dataset_path_dict, f)
+        print(minhash_dataset_path_dict)
+        print(f"Wrote the file to {args.output_file}")
+
+
+
+
